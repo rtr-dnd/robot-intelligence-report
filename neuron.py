cc0c43c559af8f8c1026257622de969c642ade6f
rtr-dnd <skysoyn@gmail.com>
2021-01-01 21:16:28 +0900

    :sparkles: multiple layers working

--- neuron.py
+++ neuron.py
@@ -14,7 +14,6 @@ class perceptron:
 
   def calc(self, x_vec):
     u = np.sum(np.multiply(x_vec, self.w_vec))
-    print(act_func(u))
     return act_func(u)
 
   def train(self, x_vec, d: float, kappa: float):
@@ -22,10 +21,13 @@ class perceptron:
     self.w_vec -= kappa * (self.calc(x_vec) - d) * x_vec
     print('trained')
 
-class layer:
-  perceptrons = []
+class input_layer:
+  def __init__(self, input_vec):
+    self.y_vec = input_vec
 
+class layer:
   def __init__(self, prev_node_num: int, percep_num: int):
+    self.perceptrons = []
     for i in range(0, percep_num):
       self.perceptrons.append(perceptron(prev_node_num))
     self.y_vec = np.zeros(percep_num)
@@ -34,12 +36,25 @@ class layer:
     for i in range(0, len(self.perceptrons)):
       self.y_vec[i] = self.perceptrons[i].calc(prev_y_vec)
 
-
-layer1 = layer(5, 3)
-layer0_y_vec = np.random.rand(5)
+network_size = [8, 5, 1] # 0番目: 入力の数
+layer0_y_vec = np.random.rand(network_size[0])
+layers = []
+layers.append(input_layer(layer0_y_vec))
+for i in range(1, len(network_size)):
+  layers.append(layer(network_size[i-1], network_size[i]))
 print(layer0_y_vec)
-layer1.forward(layer0_y_vec)
-print(layer1.y_vec)
+for i in range(1, len(network_size)):
+  print('-----')
+  print(i)
+  layers[i].forward(layers[i-1].y_vec)
+  print(layers[i].y_vec)
+print(layers[-1].y_vec)
+
+# layer1 = layer(5, 3)
+# layer0_y_vec = np.random.rand(5)
+# print(layer0_y_vec)
+# layer1.forward(layer0_y_vec)
+# print(layer1.y_vec)
 
 # kappa = 0.5
 
